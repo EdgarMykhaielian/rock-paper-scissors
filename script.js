e7ed1215696506d6f96239d4cf462da9226622a6
Eddy <edgarmykhaielian@gmail.com>
2022-12-03 14:56:12 +0200

    show results

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const buttons = document.querySelectorAll(".button")
+const userChoiceImg = document.querySelector(".user-choice")
+const oppChoiceImg = document.querySelector(".opp-choice")
+const iconContainer = document.querySelector(".icons")
+const resultsContainer = document.querySelector(".results")
+const resultsInfo = document.querySelector(".result")
+const stats = {'WON':0, 'LOST':0, 'DRAW':0}
 
 for (const button of buttons) {
     button.onclick = handleMove
@@ -9,14 +15,35 @@ function handleMove(event) {
     // remember user's choice
     const userChoice = event.target.classList[0]
     console.log(userChoice)
+
     // make random oponent's choice
     const oppChoice = selectRandom('rock', 'paper', 'scissors')
     console.log(oppChoice)
+
     // select a winner
     const result = getResult(userChoice, oppChoice)
     console.log(result)
+
     // show result
+    showResult(result, userChoice, oppChoice)
+    
     // update stats
+    stats[result]++
+    updateStats()
+}
+
+function updateStats(){
+    
+}
+
+function showResult(result, userChoice, oppChoice) {
+    iconContainer.hidden = true
+    resultsContainer.hidden = false
+
+    userChoiceImg.src = `assets/${userChoice}_icon.svg`
+    oppChoiceImg.src = `assets/${oppChoice}_icon.svg`
+
+    resultsInfo.textContent = result
 }
 
 function getResult(userChoice, oppChoice) {
