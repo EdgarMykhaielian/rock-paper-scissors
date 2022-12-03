a58d19fe43169adb31f29ea16455454f5d77ca7a
Eddy <edgarmykhaielian@gmail.com>
2022-12-03 14:15:25 +0200

    get result

--- script.js
+++ script.js
@@ -13,8 +13,34 @@ function handleMove(event) {
     const oppChoice = selectRandom('rock', 'paper', 'scissors')
     console.log(oppChoice)
     // select a winner
+    const result = getResult(userChoice, oppChoice)
+    console.log(result)
     // show result
     // update stats
+}
+
+function getResult(userChoice, oppChoice) {
+    //rock + rock => draw;
+    //rock + paper => lost;
+    //rock + scissors => won;
+
+    //paper + rock => won
+    //paper + paper => draw;
+    //paper + scissors => lost;
+
+    //scissors + rock => lost;
+    //scissors + paper => won;
+    //scissors + scissors => draw;
+
+    const pair = userChoice + oppChoice
+
+    if ('rockscissors paperrock scissorspaper'.includes(pair)) {
+        return 'WON'
+    } else if ('rockpaper paperscissors scissorsrock'.includes(pair)) {
+        return 'LOST'
+    } else {
+        return 'DRAW'
+    }
 }
 
 function selectRandom(...choices) {
