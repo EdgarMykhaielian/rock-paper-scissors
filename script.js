f99c36d68687fdb21c8d8884fdc1dd002b03ad0d
Eddy <edgarmykhaielian@gmail.com>
2022-12-03 12:56:30 +0200

    add script

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll(".button")
+
+for (const button of buttons) {
+    button.onclick = handleMove
+}
+
+function handleMove(event) {
+    // remember user's choice
+    // make random oponent's choice
+    // select a winner
+    // show result
+    // update stats
+}
